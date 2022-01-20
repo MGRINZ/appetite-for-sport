7e084c511aafd3597db418f3ad9b04703075c4c1
MGRINZ <24416088+MGRINZ@users.noreply.github.com>
2022-01-21 00:06:35 +0100

    Removing small serving sizes

--- app/src/main/java/com/example/apetytnasport/Algorithm/AlgorithmActivity.java
+++ app/src/main/java/com/example/apetytnasport/Algorithm/AlgorithmActivity.java
@@ -2,6 +2,7 @@ package com.example.apetytnasport.Algorithm;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 
@@ -12,6 +13,7 @@ import com.example.apetytnasport.Database.Sport;
 import com.example.apetytnasport.MainActivity;
 import com.example.apetytnasport.NoStatusBarActivity;
 import com.example.apetytnasport.R;
+import com.example.apetytnasport.SetupWizard.SetupWizardActivity;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -51,11 +53,29 @@ public class AlgorithmActivity extends NoStatusBarActivity {
                     return;
 
                 ArrayList<ArrayList<AlgorithmResult>> algorithmResults = removeSubstitutes(algorithm.getResults());
+                ArrayList<ArrayList<AlgorithmResult>> newAlgorithmResults;
 
-                Intent intent = new Intent(AlgorithmActivity.this, MainActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-                intent.putExtra("dietLists", algorithmResults);
-                startActivity(intent);
+
+                try {
+                    while(true) {
+                        newAlgorithmResults = removeBelowAverage(algorithmResults);
+                        if(compareResults(algorithmResults, newAlgorithmResults))
+                            break;
+                        algorithmResults = newAlgorithmResults;
+                    }
+
+                    Intent intent = new Intent(AlgorithmActivity.this, MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.putExtra("dietLists", newAlgorithmResults);
+                    startActivity(intent);
+                }
+                catch(Exception e) {
+                    Toast.makeText(getApplicationContext(), getResources().getString(R.string.prepare_diet_error), Toast.LENGTH_SHORT);
+                    Intent intent = new Intent(getApplicationContext(), SetupWizardActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+                    startActivity(intent);
+                }
+
             }
         });
     }
@@ -101,11 +121,54 @@ public class AlgorithmActivity extends NoStatusBarActivity {
         return newAlgorithmResults;
     }
 
+    @NonNull
+    private ArrayList<ArrayList<AlgorithmResult>> removeBelowAverage(ArrayList<ArrayList<AlgorithmResult>> algorithmResults) {
+        ArrayList<ArrayList<AlgorithmResult>> newAlgorithmResults = new ArrayList<>();
+
+        for(ArrayList<AlgorithmResult> productsList : algorithmResults) {
+            double averageServingSize = getAverageServingSize(productsList);
+            double threshold = averageServingSize / 2;
+
+            ArrayList<FoodItemAlgorithmData> newFoodItems = new ArrayList<>();
+            for (AlgorithmResult algorithmResult : productsList) {
+                if(algorithmResult.getServingSize() >= threshold)
+                    newFoodItems.add(algorithmResult.getFoodItem());
+            }
+
+            Algorithm algorithm = new AlgorithmExact(newFoodItems, protein, fat, carbohydrate);
+            algorithm.start();
+            algorithm.printResults();
+
+            newAlgorithmResults.addAll(algorithm.getResults());
+        }
+
+        return newAlgorithmResults;
+    }
+
     private double getAverageServingSize(ArrayList<AlgorithmResult> productsList) {
         double sum = 0.0;
         for(AlgorithmResult r : productsList)
             sum += r.getServingSize();
         return sum / productsList.size();
+    }
+
+    /**
+     * Compares two Algorithm results lists checking if they have the same size
+     * @param prevAlgorithmResults One of Algorithm results list
+     * @param newAlgorithmResults Another of Algorithm results list
+     * @return true if results have the same size, false otherwise
+     * @throws Exception when Algorithm results lists have different dimensions
+     */
+    private boolean compareResults(ArrayList<ArrayList<AlgorithmResult>> prevAlgorithmResults, ArrayList<ArrayList<AlgorithmResult>> newAlgorithmResults) throws Exception {
+        if(prevAlgorithmResults.size() != newAlgorithmResults.size())
+            throw new Exception("Algorithm results have different dimensions");
+
+        for(int i = 0; i < prevAlgorithmResults.size(); i++) {
+            if(prevAlgorithmResults.get(i).size() != newAlgorithmResults.get(i).size())
+                return false;
+        }
+
+        return true;
     }
 
     @Override
--- app/src/main/java/com/example/apetytnasport/MainActivity.java
+++ app/src/main/java/com/example/apetytnasport/MainActivity.java
@@ -3,17 +3,22 @@ package com.example.apetytnasport;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.apetytnasport.Algorithm.AlgorithmResult;
 import com.example.apetytnasport.Database.FoodItemAlgorithmData;
+import com.example.apetytnasport.SetupWizard.SetupWizardActivity;
 
 import org.w3c.dom.Text;
 
@@ -35,6 +40,24 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView dietView = findViewById(R.id.diet_view);
         dietView.setAdapter(new DietListAdapter());
         dietView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.new_diet) {
+            Intent intent = new Intent(getApplicationContext(), SetupWizardActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private class DietListAdapter extends RecyclerView.Adapter<DietListAdapter.DietListViewHolder> {
